1cba4da5b818d1c36c2d26f38b1e8c99211d0c57
lirimmb <64708387+lirimmb@users.noreply.github.com>
2021-07-23 16:11:06 +0200

    created header component

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
+  images:{
+    domains:[
+      "links.papareact.com",
+      "platfrom-lookaside.fbsbx.com",
+      "firebasestorage.googleapis.com"
+    ],
+  },
 }
